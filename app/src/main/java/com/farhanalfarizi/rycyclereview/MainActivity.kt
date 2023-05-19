a4d14344ee45454eaa4331aee28ffa3e48510a31
Farhan Alfarizi <frhnmfa@gmail.com>
2023-05-20 02:21:58 +0700

    update repository

--- app/src/main/java/com/farhanalfarizi/rycyclereview/MainActivity.kt
+++ app/src/main/java/com/farhanalfarizi/rycyclereview/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.farhanalfarizi.rycyclereview
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.farhanalfarizi.rycyclereview.adapter.AdapterTeamBola
 import com.farhanalfarizi.rycyclereview.databinding.ActivityMainBinding
 import com.farhanalfarizi.rycyclereview.model.Pemain
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,9 +26,34 @@ class MainActivity : AppCompatActivity() {
         listpemain.add(Pemain(nama = "Sergio Ramos García", R.drawable.ramos,"Belakang","1,84 m","Camas (Sevilla)","30 Maret 1986"))
         listpemain.add(Pemain(nama = "Zinedine Yazid Zidane", R.drawable.zidan,"Belakang","1,85 m","Marseille (Prancis))","23 Juni 1972"))
 
-        binding.list.adapter = AdapterTeamBola (this,listpemain,object : AdapterTeamBola.OnclickListener{
+        binding.list.adapter = AdapterTeamBola (this,listpemain,object : AdapterTeamBola.OnclickListener {
             override fun detailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detai_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txtNamaPemain)
+
+                    val posisi = this.findViewById<TextView>(R.id.txtPosisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txtTinggi)
+                    val tempatlahir = this.findViewById<TextView>(R.id.txtTempatlahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txtNamaPemain)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "$item?.nama"
+                    posisi.text = "$item?.posisi"
+                    tinggi.text = "$item?.tinggi"
+                    tempatlahir.text = "$item?.tempatlahir"
+                    tgllahir.text = "$item?.tgllahir"
+
+
+                    btn.setOnClickListener {
+                        this.dismiss()                    }
+
+                }.show()
+
             }
 
         })
